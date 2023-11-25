0204c263a1469642f9df56ca36db434ab3b20071
Hryb <gribvalerij6@gmail.com>
2023-11-25 16:53:33 +0200

    added temperature converter(task4)

---
+++ HrybValerii/HW4/Temperature_converter.py
@@ -0,0 +1,6 @@
+celsius_temp = float(input("Enter the temperature in Celsius: "))
+if celsius_temp >= -273.15:
+    fahrenheit_temp = (celsius_temp * 9 / 5) + 32
+    print(f"{celsius_temp}°C is equivalent to {fahrenheit_temp}°F")
+else:
+    print("Eror temperature below absolute zero -273.15°C")
