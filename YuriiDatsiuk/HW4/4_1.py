6dd2ba4b18b14e80558e4d547c600cd35ea8fa0b
yura <kentlutsk@ukr.net>
2023-11-27 01:47:42 +0200

    hw4

--- YuriiDatsiuk/HW4/4_1.py
+++ YuriiDatsiuk/HW4/4_1.py
@@ -1,1 +1,9 @@
-print("hello")
+temp_cels_user = int(input("Enter temperature in Celsius to convert to Fahrenheit\n"))
+
+min_temp = -273.15
+
+if temp_cels_user < min_temp:
+    print(f"Temperature below absolute zero {min_temp}° C")
+else:
+    F = (temp_cels_user *9/5) + 32
+    print(f"{temp_cels_user}° C is equivalent to {F}° F")    
