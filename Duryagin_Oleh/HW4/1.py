f8d2e4372e9b040d9a08898b4532ef036133e084
Oleh <oleg.durjagin@gmail.com>
2023-11-26 15:01:13 +0200

    Added hw4

---
+++ Duryagin_Oleh/HW4/1.py
@@ -0,0 +1,7 @@
+tmp_c = float(input('Enter the temperature is Celsius: '))
+tmp_f = (tmp_c * 9/5) + 32
+
+if tmp_c < -273.15:
+      print('Error: Temperature below absolute zero (-273.15°C)')
+else:
+      print(f'{tmp_c}°C is equivalent to {tmp_f}°F')
