8b23f11f7de9edd7988c9fe965f07e90162170fc
Alona <63323474+alona-ruda@users.noreply.github.com>
2023-12-02 00:24:05 +0200

    Final version of task 2

--- KachanAlona/HW6/Task2.py
+++ KachanAlona/HW6/Task2.py
@@ -1,7 +1,6 @@
 login_from_user = input('Please enter your login: ')
 correct_login = "First"
 while login_from_user.capitalize() != correct_login:
-    print(f'Sorry, the login {login_from_user} is wrong.')
-    print(login_from_user = input('Try again: '))
+    login_from_user = input(f'Sorry, the login {login_from_user} is wrong. Try again: ')
 else:
     print(f'Nice to meet you, {correct_login}!')
