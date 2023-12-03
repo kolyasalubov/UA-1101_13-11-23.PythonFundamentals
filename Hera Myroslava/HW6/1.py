79e66f9cce05a367b143c6ef3d57283630c056ae
Myroslava <mirosyagera@gmail.com>
2023-12-03 15:13:31 +0200

    added task

--- Hera Myroslava/HW6/1.py
+++
@@ -1,9 +0,0 @@
-even_numbers = [item for item in range(1, 11) if item % 2 == 0]
-
-odd_numbers = [item for item in range(1, 11) if item % 2 == 1 and item % 3 == 0]
-
-other_numbers = [item for item in range(1, 11) if item % 2 != 0 and item % 3 != 0]
-
-print("Even numbers divisible by 2: ", even_numbers)
-print("Odd numbers divisible by 3: ", odd_numbers)
-print("Numbers not divisible by 2 and 3: ", other_numbers)
--- Hera Myroslava/HW6/2.py
+++
@@ -1,9 +0,0 @@
-user_name = input ("Please, enter you name: ")
-user_login = ""
-
-while user_login != "First":
-  user_login = input ("Please, enter your login: ")
-  if user_login == "First":
-    print (f"Hello, ", user_name)
-  else:
-    print ("Incorrect login!")
