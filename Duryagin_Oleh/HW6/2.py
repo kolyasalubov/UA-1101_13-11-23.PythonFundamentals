ecef10c7f7b2220e86fad8bb151eacea934beb0a
Oleh <oleg.durjagin@gmail.com>
2023-12-03 14:33:29 +0200

    added hw6

---
+++ Duryagin_Oleh/HW6/2.py
@@ -0,0 +1,10 @@
+# Write a script that checks the login
+def login_funk(login_name: str) -> bool:
+    return True if login_name.capitalize() == "First" else False
+
+
+while login_funk(input("Enter login:")) != 1:
+    print("Wrong login name")
+print("Greet")
+
+
