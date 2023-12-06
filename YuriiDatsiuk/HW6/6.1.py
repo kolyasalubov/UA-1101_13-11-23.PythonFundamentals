c7d1ad4185b77a0c0e0a803a85bde8041696de8d
kentlutsk <55951367+kentlutsk@users.noreply.github.com>
2023-12-06 18:10:54 +0200

    remake 6.1

--- YuriiDatsiuk/HW6/6.1.py
+++ YuriiDatsiuk/HW6/6.1.py
@@ -1,15 +1,7 @@
-list_even_div_2 = []
-list_odd_div_3 = []
-list_notdiv_2_3 = []
+list_even_div_2 = [i for i in range(1, 11) if i%2 == 0 ]
+list_odd_div_3  = [i for i in range(1, 11) if i%2 != 0 and i%3 == 0]
+list_notdiv_2_3 = [i for i in range(1, 11) if i%2 != 0 and i%3 != 0]
 
-for i in range(1, 11):
-  if i%2 == 0:
-   list_even_div_2.append(i)
-  if i%3 == 0:
-   list_odd_div_3.append(i)
-  if i%2 != 0 or i%3 != 0:
-   list_notdiv_2_3.append(i)
-
-print(list_even_div_2)
-print(list_odd_div_3)
-print(list_notdiv_2_3)
+print('numbers that is even and divide by 2: ', list_even_div_2)
+print('numbers that is odd and divide by 3: ', list_odd_div_3)
+print('numbers that is not divide by 2 and 3: ', list_notdiv_2_3)
