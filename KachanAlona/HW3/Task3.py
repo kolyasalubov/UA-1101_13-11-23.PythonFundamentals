3ac23432f4372ff4166532c7be59146c76a17283
Alona <63323474+alona-ruda@users.noreply.github.com>
2023-12-07 00:31:19 +0200

    Task 3. Interchange the values of two variables without the third variable.

---
+++ KachanAlona/HW3/Task3.py
@@ -0,0 +1,8 @@
+variable_1 = 1254
+variable_2 = 234
+
+variable_1 = variable_1 + variable_2
+variable_2 = variable_1 - variable_2
+variable_1 = variable_1 - variable_2
+
+print(variable_1, variable_2)
