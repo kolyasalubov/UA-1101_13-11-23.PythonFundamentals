c944ac9b46a0daa496642de16d9af223ee0b5e06
Hryb <gribvalerij6@gmail.com>
2023-12-07 15:47:26 +0200

    added task1-2

---
+++ HrybValerii/HW7/task1.py
@@ -0,0 +1,8 @@
+def comparator(number1: int|float, number2: int|float) -> float|int: 
+    '''
+    The function takes two numbers and returns the largest of them
+    If numbers are euqal returns the first one
+    '''
+    return max(number1, number2)
+
+print(comparator(1, 1))
