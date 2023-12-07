4877d3be72b6d89bbeb3d090240f63ab72a46f73
Hryb <gribvalerij6@gmail.com>
2023-12-07 15:53:07 +0200

    added task3

---
+++ HrybValerii/HW7/task3.py
@@ -0,0 +1,7 @@
+def char_calculator(string: str) -> dict:
+    '''
+    Thefunction takes a string and returns a dictionary, where key are characters and numbers of those characters are values
+    '''
+    return {character: string.count(character) for character in set(string)}
+
+print(char_calculator('hello'))
