96e817d3e07c0a05eda8de6b4bcce178f44c9862
YaraBlack <yara26072000@gmail.com>
2023-12-10 16:39:19 +0200

    add modules with area calculations

---
+++ ChernobaiYaroslav/HW8/task3/rectangle.py
@@ -0,0 +1,3 @@
+def rectangleArea(length, width):
+    """Calculate the area of a rectangle using it's width and length."""
+    return length * width
---
+++ ChernobaiYaroslav/HW8/task3/triangle.py
@@ -0,0 +1,3 @@
+def triangleArea(base, height):
+    """Calculate the area of a triangle using it's base and height."""
+    return base * height / 2
