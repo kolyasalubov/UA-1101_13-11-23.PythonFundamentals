7f8b73ceb37db076627dfbcf74cb5ae35c0b3ea8
Anna Riznychok <annariznychok@gmail.com>
2023-12-11 22:30:55 +0200

    creat folder 08_01_tasks

---
+++ RiznychokAnna/HW7/07_codewares.py
@@ -0,0 +1,6 @@
+# Practical Tasks #1 Jenny has written a function that returns a greeting for a user. 
+
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    return f"Hello, {name}!"
