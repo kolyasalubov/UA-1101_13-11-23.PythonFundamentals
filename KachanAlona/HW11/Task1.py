e23cae9a827c4837aa6c3c13742807d2445ea35b
Alona <63323474+alona-ruda@users.noreply.github.com>
2023-12-20 01:44:07 +0200

    Task 1, 2

--- KachanAlona/HW11/Task1.py
+++ KachanAlona/HW11/Task1.py
@@ -1,12 +1,16 @@
-try:
-    age = int(input('Your age: '))
-    if age < 0:
-        raise ValueError('Age should be positive')
-except ValueError as e:
-    print('Age should be positive')
+def age_checker():
+    try:
+        age = int(input('Your age: '))
+        if age < 0:
+            raise ValueError('Age should be positive')
+    except ValueError as e:
+        print(e)
 
-else:
-    if age % 2 == 0:
-        print('Age is even.')
     else:
-        print('Age is odd.')
+        if age % 2 == 0:
+            print('Age is even.')
+        else:
+            print('Age is odd.')
+
+
+age_checker()
