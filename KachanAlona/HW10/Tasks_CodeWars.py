2a6937daa928fdf1f16401e96d675daaa13b32b0
Ruda <ruda.lisa.ua@gmail.com>
2023-12-19 18:53:45 +0200

    CodeWars V

--- KachanAlona/HW10/Tasks_CodeWars.py
+++ KachanAlona/HW10/Tasks_CodeWars.py
@@ -20,7 +20,35 @@ class Ghost(object):
 # III. Basic-subclasses-Adam-and-Eve
 
 # IV. Classy-classes
-#
+
+
 # V. Building Spheres
-#
+class Sphere(object):
+    """Constructor of Sphere. Calculates volume, surface area and density."""
+    def __init__(self, radius, mass):
+        self.radius = radius
+        self.mass = mass
+
+    def get_radius(self):
+        return self.radius
+
+    def get_mass(self):
+        return self.mass
+
+    def get_volume(self):
+        from math import pi
+        self.volume = 4 / 3 * pi * self.radius ** 3
+        return round(self.volume, 5)
+
+    def get_surface_area(self):
+        from math import pi
+        surf_area = 4 * pi * self.radius ** 2
+        return round(surf_area, 5)
+
+    def get_density(self):
+        from math import pi
+        density = self.mass / self.volume
+        return round(density, 5)
+
+
 # VI. Dynamic Classes
