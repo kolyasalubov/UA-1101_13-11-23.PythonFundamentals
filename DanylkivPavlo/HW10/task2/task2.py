dfb7e677f40a1477e7ab75239338f99975247516
Pavlo Danylkiv <danylkiv.pavlo@gmail.com>
2023-12-20 03:37:50 +0200

    Added the second task

---
+++ DanylkivPavlo/HW10/task2/task2.py
@@ -0,0 +1,22 @@
+class Human:
+    species = "Homosapiens"
+
+    def __init__(self, name):
+        self.name = name
+
+    def say_hello(self):
+        return f"Hello, {self.name}! Welcome!"
+
+    @classmethod
+    def get_species(cls):
+        return cls.species
+
+    @staticmethod
+    def arbitrary_message():
+        return "This is a static method with an arbitrary message."
+
+
+person = Human("Tom")
+print(person.say_hello())
+print(f"Species: {Human.get_species()}")
+print(Human.arbitrary_message())
