337756d6b45fccdfed27f209c6818a385b4ed30e
Michael <michaelpost1984@gmail.com>
2024-01-05 23:07:04 +0200

    Added all HW8 tasks

---
+++ MomotiukMykhailo/HW8/task 8.1/models/__init__.py
@@ -0,0 +1,4 @@
+from models.user import *
+from models.admin import *
+
+__all__ = ['create_user', 'create_admin']
---
+++ MomotiukMykhailo/HW8/task 8.1/utils/__init__.py
@@ -0,0 +1,4 @@
+from utils.formatter import *
+from utils.logger import *
+
+__all__ = ['log_in_file', 'format_string']
