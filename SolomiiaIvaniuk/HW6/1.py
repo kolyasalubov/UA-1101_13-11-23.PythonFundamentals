52f9bab7544a7ebc815e39bbce23deab903806d3
Solomiia <isolomia207@gmail.com>
2024-01-13 01:47:05 +0200

    added file 1.py

--- SolomiiaIvaniuk/HW6/1.py
+++ SolomiiaIvaniuk/HW6/1.py
@@ -1,10 +1,10 @@
-lst_of_integer = [1,2,3,4,5,6]
-lst_of_float = []
+number1 = [number for number in range(1, 10) if number % 2 == 0]
+number2 = [number for number in range(1, 10) if number % 2 != 0 and number % 3 == 0]
+number3 = [number for number in range(1, 10) if number % 2 != 0 and number % 3 != 0]
 
-for i in lst_of_integer:
-    lst_of_float.append(float(i))
-print(f"List of elements floating type: ",lst_of_float)
-print(f"List of elements integer type: ",lst_of_integer)
+print("Even numbers divisible by 2:", number1)
+print("Odd numbers divisible by 3:", number2)
+print("Numbers not divisible by 2 and 3:", number3)
 
 
 
